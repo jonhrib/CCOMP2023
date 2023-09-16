2375682f16d3f3d666f9a6ef72c6fbc546a429cd
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2023-09-16 12:13:30 -0300

    Add files via upload

---
+++ Transições.java
@@ -0,0 +1,30 @@
+package MaquinaDeTuring;
+import java.util.Scanner;
+
+public class Transições {
+	private String vetaux[] = new String [3];
+	Scanner s = new Scanner (System.in);
+	
+	public Transições (String estadofuturo, String alfabetofuturo, String direção) {
+		this.vetaux[0] = estadofuturo;
+		this.vetaux[1] = alfabetofuturo;
+		if (direção.equalsIgnoreCase("D") || direção.equalsIgnoreCase("E") || direção.equalsIgnoreCase("x")) this.vetaux[2] = direção;
+		else {
+			int teste = 0;
+			while (teste == 0) {
+			System.out.print("A direção informada não é valida, digite D ou E: "); this.vetaux[2]=s.next();
+			if (this.vetaux[2].equalsIgnoreCase("D") || this.vetaux[2].equalsIgnoreCase("E") || this.vetaux[2].equalsIgnoreCase("x")) teste = 1;
+			}
+		}
+	}
+
+	public String[] getVetaux() {
+		return vetaux;
+	}
+
+	public void setVetaux(String[] vetaux) {
+		this.vetaux = vetaux;
+	}
+	
+	
+}
