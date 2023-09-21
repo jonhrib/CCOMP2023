5d2cb3fde28113ddc1c36a9ce76c54ab058a25eb
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2023-09-21 11:06:17 -0300

    Exercício - beecrowd

---
+++ beecrowd/Bee3475.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Bee3475 {
+
+	public static void main (String args []) {
+		Scanner s = new Scanner (System.in);
+		
+		String entrada = s.next();
+		
+		switch (entrada) {
+		
+		case ("0"): System.out.println("zero");
+			break;
+		case ("1"): System.out.println("um");
+			break;
+		case ("2"): System.out.println("dois");
+			break;
+		case ("3"): System.out.println("tres");
+			break;
+		case ("4"): System.out.println("quatro");
+			break;
+		case ("5"): System.out.println("cinco");
+			break;
+		case ("6"): System.out.println("seis");
+			break;
+		case ("7"): System.out.println("sete");
+			break;
+		case ("8"): System.out.println("oito");
+			break;
+		case ("9"): System.out.println("nove");
+			break;
+		case ("zero"): System.out.println("0");
+			break;
+		case ("um"): System.out.println("1");
+			break;
+		case ("dois"): System.out.println("2");
+			break;
+		case ("tres"): System.out.println("3");
+			break;
+		case ("quatro"): System.out.println("4");
+			break;
+		case ("cinco"): System.out.println("5");
+			break;
+		case ("seis"): System.out.println("6");
+			break;
+		case ("sete"): System.out.println("7");
+			break;
+		case ("oito"): System.out.println("8");
+			break;
+		case ("nove"): System.out.println("9");
+			break;
+		
+		}
+	}
+}
