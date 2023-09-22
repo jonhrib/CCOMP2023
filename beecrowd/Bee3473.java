0a2b5364b42c80a94feb0e82b972365dd0d3e84c
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2023-09-21 22:34:37 -0300

    Exercícios - beecrowd

---
+++ beecrowd/Bee3473.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Bee3473 {
+
+	public static void main (String args []) {
+		Scanner s = new Scanner (System.in);
+		
+		long dado = s.nextLong();
+		System.out.println((dado+1));
+	}
+}
