d4d3af8fdfc432e015a123bce9bc88f2a4b1c74a
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2023-09-24 15:26:02 -0300

    Exercício - beecrowd

---
+++ beecrowd/Bee3397.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Bee3397 {
+
+	public static void main (String args []) {
+		Scanner s = new Scanner (System.in);
+		
+		String frase = s.nextLine();
+		System.out.println("eu");
+	}
+}
