443456fb5446c668e0bec845ddce16af68212a26
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2023-10-29 21:08:13 -0300

    Update Produções.java

--- GramáticasRegulares/Produções.java
+++ GramáticasRegulares/Produções.java
@@ -1,71 +1,73 @@
 package Objeto;
 
-public class Produções {
+public class Produções { // classe Produções 
 	
-	private char futuro;
-	private char atual;
-	private char terminal;
-	private String total;
+	private char futuro; // atributo futuro que irá receber a variável futura da ordem de produção
+	private char atual; // atributo atual que irá receber a variável atual da ordem de produção
+	private char terminal; // atributo terminal que irá receber o terminal da ordem de produção
+	private String total; // atributo total que receberá a ordem de produção inteira
 //	private int passou;
-	private int pos;
+	private int pos; // atributo pos que receberá a posição em que a ordem foi recebida
 	
 	
-	public Produções(String ordem, int tipo) {
-		if (tipo == 1) {
-			this.pos = 0;
+	public Produções(String ordem, int tipo) { // construtor que recebe a ordem em String e o tipo (GLUD ou GLUE)
+		
+		if (tipo == 1) { // se GLUD
+			this.pos = 0; // a posição começa como 0 inicialmente
 		//	this.passou = 0;
-			this.futuro = ordem.charAt(3);
-			this.atual = ordem.charAt(0);
-			this.terminal = ordem.charAt(2);
-			this.total = ordem;
+			this.futuro = ordem.charAt(3); // futuro recebe a posição 3 da ordem, ou seja, a última
+			this.atual = ordem.charAt(0); // atual recebe a posição 0 da ordem, ou seja, a primeira
+			this.terminal = ordem.charAt(2); // terminal recebe a posição 2 da ordem, ou seja, a penúltima
+			this.total = ordem; // total recebe a ordem completa
 		}
-		else if (tipo == 2) {
-			this.pos = 0;
+		
+		else if (tipo == 2) { // se GLUE
+			this.pos = 0; // a posição começa como 0 inicialmente
 		//	this.passou = 0;
-			this.futuro = ordem.charAt(2);
-			this.atual = ordem.charAt(0);
-			this.terminal = ordem.charAt(3);
-			this.total = ordem;
+			this.futuro = ordem.charAt(2); // futuro recebe a posição 2 da ordem, ou seja, a penúltima
+			this.atual = ordem.charAt(0); // atual recebe a posição 0 da ordem, ou seja, a primeira
+			this.terminal = ordem.charAt(3); // terminal receba a posição 3 da ordem, ou seja, a última
+			this.total = ordem; // total recebe a ordem completa
 		}
 	}
 
 
-	public char getFuturo() {
+	public char getFuturo() { // get de futuro
 		return futuro;
 	}
 
 
-	public void setFuturo(char futuro) {
+	public void setFuturo(char futuro) { // set de futuro
 		this.futuro = futuro;
 	}
 
 
-	public char getAtual() {
+	public char getAtual() { // get de atual
 		return atual;
 	}
 
 
-	public void setAtual(char atual) {
+	public void setAtual(char atual) { // set de atual
 		this.atual = atual;
 	}
 
 
-	public char getTerminal() {
+	public char getTerminal() { // get de terminal
 		return terminal;
 	}
 
 
-	public void setTerminal(char terminal) {
+	public void setTerminal(char terminal) { // set de terminal
 		this.terminal = terminal;
 	}
 
 
-	public String getTotal() {
+	public String getTotal() { // get de total
 		return total;
 	}
 
 
-	public void setTotal(String total) {
+	public void setTotal(String total) { // set de total
 		this.total = total;
 	}
 
@@ -80,12 +82,12 @@ public class Produções {
 //	}
 
 
-	public int getPos() {
+	public int getPos() { // get de pos
 		return pos;
 	}
 
 
-	public void setPos(int pos) {
+	public void setPos(int pos) { // set de pos
 		this.pos = pos;
 	}
 }
