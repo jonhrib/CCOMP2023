4718d90c7cab8db77958b976670dcb88af3b9140
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2023-11-06 15:24:39 -0300

    Classe Principal - Interface

---
+++ ProgramaçãoOrientadaAObjetos/Interface/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import uicodigo.UIPrincipal;
+
+public class Principal {
+	
+	public static void main (String args []) {
+		UIPrincipal principal = new UIPrincipal();
+		principal.setVisible(true);
+		
+	}
+
+}
