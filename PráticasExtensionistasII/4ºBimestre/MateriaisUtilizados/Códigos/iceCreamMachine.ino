e705c0d3e6dbe54e152ecbe0032627e6578402ce
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-03-02 15:16:59 -0300

    Código Arduíno - Máquina de Sorvetes

---
+++ PráticasExtensionistasII/4ºBimestre/MateriaisUtilizados/Códigos/iceCreamMachine.ino
@@ -0,0 +1,61 @@
+#define chocolatteLevelSensor 2
+#define strawberryLevelSensor 3
+#define chocolattePushButton 4
+#define strawberryPushButton 5
+#define chocolatteLightIndicator 6
+#define strawberryLightIndicator 7
+#define chocolattePump 8
+#define strawberryPump 10
+
+bool chocolatteContainer;
+bool chocolatteButtonReleaser;
+bool strawberryContainer;
+bool strawberryButtonReleaser;
+
+void setup() {
+  pinMode(chocolatteLevelSensor, INPUT_PULLUP);
+  pinMode(strawberryLevelSensor, INPUT_PULLUP);
+  pinMode(chocolattePushButton, INPUT_PULLUP);
+  pinMode(strawberryPushButton, INPUT_PULLUP);
+  pinMode(chocolatteLightIndicator, OUTPUT);
+  pinMode(strawberryLightIndicator, OUTPUT);
+  pinMode(chocolattePump, OUTPUT);
+  pinMode(strawberryPump, OUTPUT);
+}
+
+void loop() {
+  chocolatteContainer = digitalRead(chocolatteLevelSensor);
+  chocolatteButtonReleaser = digitalRead(chocolattePushButton);
+  strawberryContainer = digitalRead(strawberryLevelSensor);
+  strawberryButtonReleaser = digitalRead(strawberryPushButton);
+
+
+  if (chocolatteContainer == LOW) {
+    if (chocolatteButtonReleaser == LOW) {
+      digitalWrite(chocolattePump, LOW);
+    }
+    else {
+      digitalWrite(chocolattePump, LOW);
+    }
+    digitalWrite(chocolatteLightIndicator, LOW);
+  }
+  else {
+    digitalWrite(chocolatteLightIndicator, LOW);
+    digitalWrite(chocolattePump, HIGH);
+  }
+
+
+  if (strawberryContainer == HIGH) {
+    if (strawberryButtonReleaser == HIGH) {
+      digitalWrite(strawberryPump, LOW);
+    }
+    else {
+      digitalWrite(strawberryPump, HIGH);
+    }
+    digitalWrite(strawberryLightIndicator, LOW);
+  }
+  else {
+    digitalWrite(strawberryLightIndicator, HIGH);
+    digitalWrite(strawberryPump, HIGH);
+  }
+}
